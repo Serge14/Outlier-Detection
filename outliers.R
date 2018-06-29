b5961934d1c9af9b578fdb35b037b48993d45b94
Serge14 <sobukhov@gmail.com>
2018-06-29 23:36:48 +0300

    update

--- outliers.R
+++ outliers.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(zoo)
 library(data.table)
 
-outliersDetection = function(x, periodsWindow = 6, threshold = 1) {
+outliersDetection = function(x, periodsWindow = 4, threshold = 1) {
   df = as.data.table(x)
   names(df)[1] = "y"
   df[, `:=`(n = seq(1:nrow(df)),
@@ -18,20 +18,31 @@ outliersDetection = function(x, periodsWindow = 6, threshold = 1) {
   df[, residuals2 := y - yadj]
   df[, outliers2 := residuals2^2/sd(yadj)^2]
   
+  print("Outliers:")
   print(df[outliers2 > threshold, .(y, round(outliers2, 2))])
   
   ggplot(df, aes(x = n)) + 
-    geom_line(aes(y = y), colour = "black") + 
-    geom_line(aes(y = yav), colour = "royalblue2") +
-    geom_line(aes(y = yadj), colour = "red") +
-    geom_point(data = df[outliers1 > threshold,], aes(x = n, y = y), colour="royalblue2", size=3) +
-    geom_point(data = df[outliers2 > threshold,], aes(x = n, y = y), colour="red", size=1.5) +
+    geom_line(aes(y = y, colour = "original")) + 
+    geom_line(aes(y = yav, colour = "averaged")) +
+    geom_line(aes(y = yadj, colour = "smoothed")) +
+    geom_point(data = df[outliers1 > threshold,], 
+               aes(x = n, y = y, fill = "1st type"), colour="royalblue2", size=3) +
+    geom_point(data = df[outliers2 > threshold,], 
+               aes(x = n, y = y, fill = "2nd type"), colour="red", size=1.5) +
     theme_minimal() + 
     labs(title = "Outliers Detection in Time Series", 
          subtitle = paste0("periods window = ", periodsWindow, ", score > ", threshold),
          x = "Periods", y = "Value") +
     theme(axis.title.y = element_text(angle=0, vjust = 1),
           axis.title.x = element_text(hjust = 1)) + 
-    theme(legend.position=c(1, 1)) +
-    scale_colour_manual(values = c("red", "blue", "green"))
+#    theme(legend.position=c(1, 1)) +
+    scale_color_manual("Series", 
+                    breaks = c("original", "averaged", "smoothed"),
+                    values = c("royalblue2", "black", "red")) +
+    guides(color = guide_legend(override.aes = list(linetype = "solid",
+                                                color = c("black", "royalblue2", "red"))),
+         fill = guide_legend(override.aes = list(shape = 19, 
+                                                color = c("royalblue2", "red")))) +
+    scale_fill_discrete("Outliers")
+  
 }
